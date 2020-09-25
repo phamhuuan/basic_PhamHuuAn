dfe812de978ff69a059c2b04f6d43dc2163f6322
Pham Huu An <anph@deepcare.io>
2020-09-25 16:05:03 +0700

    update

--- src/Week2/Kadai4.rb
+++ src/Week2/Kadai4.rb
@@ -1,8 +1,8 @@
 print "数字を入力してください："
 input = gets.chomp
-number = input.to_f
+number = input.to_i
 
-if number.to_i.to_f == number && number.to_s == input
+if number.to_s == input
   if number.to_i % 14 == 0
     puts "14の倍数です"
   elsif number.to_i % 7 == 0
