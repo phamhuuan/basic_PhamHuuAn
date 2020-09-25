9aa9e6ee724f27a272e46ae33d7fe382a2dec13b
Pham Huu An <anph@deepcare.io>
2020-09-25 14:47:59 +0700

    update

--- src/Week2/Kadai1.rb
+++ src/Week2/Kadai1.rb
@@ -1,14 +1,14 @@
 time = Time.now
-currentYear = time.year
-ignoreYear = [1916, 1940, 1944]
-startYear = 1896
+current_year = time.year
+ignore_year = [1916, 1940, 1944]
+start_year = 1896
 year = gets.chomp.to_i
 
-if year > currentYear
-  puts "Current year is #{currentYear}. I don't know Olympic will occur or not :>!"
-elsif year < startYear
-  puts "Lew lew the first Olympic occurred in #{startYear}!"
-elsif ignoreYear.include?(year)
+if year > current_year
+  puts "Current year is #{current_year}. I don't know Olympic will occur or not :>!"
+elsif year < start_year
+  puts "Lew lew the first Olympic occurred in #{start_year}!"
+elsif ignore_year.include?(year)
   puts "Oops in this year, for some reasons Olympic was not occurred!"
 elsif year % 4 == 0
   puts "Yay, congrats in #{year} Olympic was occurred!"
