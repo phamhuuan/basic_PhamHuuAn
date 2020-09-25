af0dc903b68dee7e175cd90ac77d0479aaef4fb6
Pham Huu An <anph@deepcare.io>
2020-09-25 16:02:48 +0700

    update

--- src/Week2/Kadai3.rb
+++ src/Week2/Kadai3.rb
@@ -1,17 +1,20 @@
 olympic = [
     {:year => 1896, :city => "アテネ"},
     {:year => 1900, :city => "パリ"},
-    {:year => 1904, :city => "セントルイス"},
+    {:year => 1904, :city => "セントルイス", :note => "アメリカ初開催"},
     {:year => 1908, :city => "ロンドン"},
     {:year => 1912, :city => "ストックホルム"},
-    {:year => 1916, :city => "ベルリン"},
+    {:year => 1916, :city => "ベルリン", :note => "第一次世界大戦で中止"},
     {:year => 1920, :city => "アントワープ"},
-    {:year => 1924, :city => "パリ"},
+    {:year => 1924, :city => "パリ", :note => "同じ都市での２回目の開催は初"},
     {:year => 1928, :city => "アムステルダム"},
     {:year => 1932, :city => "ロサンゼルス"},
 ]
 
 olympic.each do |item|
   puts "#{item[:year]}年#{item[:city]}大会"
-  puts "----------------------------------"
+  if item[:note]
+    puts "豆知識：#{item[:note]}"
+  end
+  puts "ーーーーーーーーーーーーーーーーーーー"
 end
---
+++ src/Week2/Kadai4.rb
@@ -0,0 +1,17 @@
+print "数字を入力してください："
+input = gets.chomp
+number = input.to_f
+
+if number.to_i.to_f == number && number.to_s == input
+  if number.to_i % 14 == 0
+    puts "14の倍数です"
+  elsif number.to_i % 7 == 0
+    puts "7の倍数です"
+  elsif number.to_i % 2 == 0
+    puts "2の倍数です"
+  else
+    puts "2の倍数でも7の倍数でもありません"
+  end
+else
+  puts "整数ではありません"
+end
